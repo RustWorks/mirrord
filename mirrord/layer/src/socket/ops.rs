d72ec866dc3886c7922fbad4f432438c8a9e4d8a
Aviram Hassan <aviram@metalbear.co>
2024-05-09 13:39:45 +0300

    aa

--- mirrord/layer/src/socket/ops.rs
+++ mirrord/layer/src/socket/ops.rs
@@ -255,12 +255,14 @@ pub(super) fn bind(
     // Check if the user's requested address isn't already in use, even though it's not actually
     // bound, as we bind to a different address, but if we don't check for this then we're
     // changing normal socket behavior (see issue #1123).
-    if SOCKETS.iter().any(|socket| match &socket.state {
-        SocketState::Initialized | SocketState::Connected(_) => false,
-        SocketState::Bound(bound) | SocketState::Listening(bound) => {
-            bound.requested_address == requested_address
-        }
-    }) {
+    if requested_address.port() != 0
+        && SOCKETS.iter().any(|socket| match &socket.state {
+            SocketState::Initialized | SocketState::Connected(_) => false,
+            SocketState::Bound(bound) | SocketState::Listening(bound) => {
+                bound.requested_address == requested_address
+            }
+        })
+    {
         Err(HookError::AddressAlreadyBound(requested_address))?;
     }
 
